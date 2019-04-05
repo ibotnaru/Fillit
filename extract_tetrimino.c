86310487daa3c6d34c6ad56d6fb916651b863527
Ina Botnaru <ibotnaru@e1z2r7p29.42.us.org>
2019-04-04 21:23:59 -0700

    in progress

--- extract_tetrimino.c
+++ extract_tetrimino.c
@@ -6,7 +6,7 @@
 /*   By: ibotnaru <ibotnaru@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/04/04 13:44:50 by ibotnaru          #+#    #+#             */
-/*   Updated: 2019/04/04 17:44:13 by ibotnaru         ###   ########.fr       */
+/*   Updated: 2019/04/04 21:12:47 by ibotnaru         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,26 +14,41 @@
 #include "libft.h"
 
 //<<<<<<<<<<<<<<<<<<<<<<<STEP 12>>>>>>>>>>>>>>>>>>>>>>>>>>>>
-t_tetr    *extract_tetr(char   **tetr_stor)
+static  t_tetr    *extract_tetr(char   **tetr_stor)
 {
     int     y;
     int     x;
     t_tetr  *block_of_hashes;                                                //this block comtains the positions of the #-es(4 of them)
+    int     i;
+    int     j;
+    int     counter;
 
     y = 0;
     x = 0;
-    while (tetr_stor[y][x])
+    i = 0;
+    j = 0;
+    counter = 0;
+    while (counter < 21)
     {
         x = 0;
+        i = 0;
+        j = 0;
         while (tetr_stor[y][x] != '\n')
         {
-            //if we found a # we sent the position to the structure
+            //if we found a # we sent the position to the structure block_of_hashes
             if (tetr_stor[y][x] == '#')
             {
-                block_of_hashes->x = x;
+                block_of_hashes->x[i] = x;
+                block_of_hashes->y[j] = y;
+                i++;
+                j++;
             }
+            x++;
         }
+        y++;
+        counter++;
     }
+    return (block_of_hashes);
 }
 
 //<<<<<<<<<<<<<<<<<<<<<<<STEP 11>>>>>>>>>>>>>>>>>>>>>>>>>>>>
@@ -53,7 +68,11 @@ t_tetr    **storing_tetr(char   *tetr_stor)
     storage = (t_tetr **)malloc(sizeof(t_tetr) * num_of_tetrs);              //we alloc memory size of tetr_stor for storage and it has enough space for all tetriminios
     while (counter < num_of_tetrs)
     {
-        
+        storage[counter] = extract_tetr(tetr_stor);
+        storage[counter]->name_for_tetr = name_of_tetr;
+        counter++;
+        name_of_tetr++;
     }
-
+    storage[counter] = '\0';                                                 //to avoid leaks
+    return(storage);
 }
